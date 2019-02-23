a9403d82ece918865873b1643449e686ce2cd5b7
Harmeet Singh <hsbhatia765@outlook.com>
2019-02-23 14:27:39 +0530

    Update announce.js

--- announce.js
+++ announce.js
@@ -8,7 +8,7 @@ bot.on('ready',()=>{
         title: 'Announcement',
         thumbnail: bot.user.avatarURL,
     })
-    let text = `I've finally been updated guys!!\n\nListed below are some of the new features!\n=> New commands!\n=> Better data management\n=> Improvement in performance\n=> Improvement of old commands\n=> And much more!\n\n\nUse \`=help\` for more help.\n\n You can also use \`=direct-help\` for getting help via DMs.\n\nPlease do check these new things out and thanks for the patience again.`
+    let text = `There's a new update guys!\nHere's a link to my new website: https://Trendy.thedrone7.repl.co.\n\nBe sure to check it out and also, use the \`=suggest\` command or the support webpage on the website to suggest a new feature that you would like to see :smile:\n\nThanks and C ya.`
     announcement.setTimestamp().setDescription(text).setColor("RANDOM")
     bot.channels.filter(chan=> chan.type=='text' && (chan.name.toLowerCase().indexOf('chat') > -1 || chan.name.toLowerCase().indexOf('general') > -1)).forEach(chan=>{
         chan.send(announcement).then(msg=>{
